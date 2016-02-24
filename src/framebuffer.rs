6bb5f97c23fced2ee309b7c26d8a96eafb6b76c8
Niilo Jaba <niilo.jaba@gmail.com>
2016-02-24 02:15:16 +0200

    Changed 4-space tabs to actual tabs

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -2,72 +2,58 @@ use sdl2;
 use sdl2::SdlResult;
 
 pub struct Framebuffer<'a> {
-    window : sdl2::video::Window,
-    surface : sdl2::surface::Surface<'a>,
+	window : sdl2::video::Window,
+	surface : sdl2::surface::Surface<'a>,
 }
 
 impl<'a> Framebuffer<'a> {
-    pub fn new(video_ctx : &sdl2::VideoSubsystem, width : u32, height : u32) -> SdlResult<Framebuffer<'a>> {
-        let mut window = try!(video_ctx.window("Doom", width, height).position_centered().build());
-        window.show();
+	pub fn new(video_ctx : &sdl2::VideoSubsystem, width : u32, height : u32) -> SdlResult<Framebuffer<'a>> {
+		let mut window = try!(video_ctx.window("Doom", width, height).position_centered().build());
+		window.show();
 
-        Ok(Framebuffer {
-            window: window,
-            surface: try!(sdl2::surface::Surface::new(width, height, sdl2::pixels::PixelFormatEnum::BGRX8888)),
-        })
-    }
+		Ok(Framebuffer {
+			window: window,
+			surface: try!(sdl2::surface::Surface::new(width, height, sdl2::pixels::PixelFormatEnum::BGRX8888)),
+		})
+	}
 
-    pub fn get(&mut self) -> (&mut [u8], usize, usize, usize) {
-        let width = self.surface.width() as usize;
-        let height = self.surface.height() as usize;
-        let pitch = self.surface.pitch() as usize;
+	pub fn get(&mut self) -> (&mut [u8], usize, usize, usize) {
+		let width = self.surface.width() as usize;
+		let height = self.surface.height() as usize;
+		let pitch = self.surface.pitch() as usize;
 
-        // If this panics, fix the surface creation code.
-        let buffer = self.surface.without_lock_mut().unwrap();
+		// If this panics, fix the surface creation code.
+		let buffer = self.surface.without_lock_mut().unwrap();
 
-        (buffer, width, height, pitch)
-    }
+		(buffer, width, height, pitch)
+	}
 
-    pub fn clear(&mut self, r : u8, g : u8, b : u8) {
-        let (screen, width, height, pitch) = self.get();
+	pub fn clear(&mut self, r : u8, g : u8, b : u8) {
+		let (screen, width, height, pitch) = self.get();
 
-        for y in 0..height {
-            for x in 0..width {
-                screen[y * pitch + x * 4 + 0] = 0x00u8;
-                screen[y * pitch + x * 4 + 1] = r;
-                screen[y * pitch + x * 4 + 2] = g;
-                screen[y * pitch + x * 4 + 3] = b;
-            }
-        }
-    }
+		for y in 0..height {
+			for x in 0..width {
+				screen[y * pitch + x * 4 + 0] = 0x00u8;
+				screen[y * pitch + x * 4 + 1] = r;
+				screen[y * pitch + x * 4 + 2] = g;
+				screen[y * pitch + x * 4 + 3] = b;
+			}
+		}
+	}
 
-    pub fn present(&self, event_pump : &sdl2::EventPump) -> bool {
-        let surface = match self.window.surface(event_pump) {
-            Ok(value) => value,
-            Err(_) => return false
-        };
+	pub fn present(&self, event_pump : &sdl2::EventPump) -> bool {
+		let surface = match self.window.surface(event_pump) {
+			Ok(value) => value,
+			Err(_) => return false
+		};
 
-        // Stupid SDL wrapper isn't implementing AsMut properly, so we need to do
-        // unsafe hack.
-        unsafe {
-            let _ = self.surface.blit(None, sdl2::surface::Surface::from_ll(surface.raw()), None);
-        }
+		// Stupid SDL wrapper isn't implementing AsMut properly, so we need to do
+		// unsafe hack.
+		unsafe {
+			let _ = self.surface.blit(None, sdl2::surface::Surface::from_ll(surface.raw()), None);
+		}
 
-        self.window.update_surface().unwrap();
-        true
-    }
+		self.window.update_surface().unwrap();
+		true
+	}
 }
-
-/*
-fn clamp<T : PartialOrd>(value : T, min_value : T, max_value : T) -> T
-{
-    if value <= min_value {
-        min_value
-    } else if value >= max_value {
-        max_value
-    } else {
-        value
-    }
-}
-*/
-
--- src/resources/resourcemanager.rs
+++ src/resources/resourcemanager.rs
@@ -13,93 +13,93 @@ use resources::WadError;
 pub type PackageLoadResult<T> = Result<T, PackageLoadError>;
 
 enum PackageFormat {
-    Unknown,
-    IWad,
-    PWad
+	Unknown,
+	IWad,
+	PWad
 }
 
 #[derive(Debug)]
 pub enum PackageLoadError {
-    UnknownPackage,
-    IoFailure(io::Error),
-    WadError(WadError)
+	UnknownPackage,
+	IoFailure(io::Error),
+	WadError(WadError)
 }
 
 pub struct ResourceManager {
-    maps : Vec<bsp::Map>
+	maps : Vec<bsp::Map>
 }
 
 impl ResourceManager {
-    pub fn new() -> ResourceManager {
-        ResourceManager {
-            maps: Vec::<bsp::Map>::new()
-        }
-    }
+	pub fn new() -> ResourceManager {
+		ResourceManager {
+			maps: Vec::<bsp::Map>::new()
+		}
+	}
 
-    pub fn find_map(&self, name : &str) -> Option<&bsp::Map> {
-        for level in &self.maps {
-            if level.name == name {
-                return Some(level);
-            }
-        }
-        return None;
-    }
+	pub fn find_map(&self, name : &str) -> Option<&bsp::Map> {
+		for level in &self.maps {
+			if level.name == name {
+				return Some(level);
+			}
+		}
+		return None;
+	}
 
-    pub fn load_package<P : AsRef<Path>>(&mut self, path : P) -> PackageLoadResult<()> {
-        let mut file = match File::open(path) {
-            Ok(value) => value,
-            Err(err) => return Err(PackageLoadError::IoFailure(err))
-        };
+	pub fn load_package<P : AsRef<Path>>(&mut self, path : P) -> PackageLoadResult<()> {
+		let mut file = match File::open(path) {
+			Ok(value) => value,
+			Err(err) => return Err(PackageLoadError::IoFailure(err))
+		};
 
-        match try!(detect_package_format(&mut file)) {
-            PackageFormat::IWad | PackageFormat::PWad => {
-                self.add_package(&try!(wrap_wad_error(WadPackage::new(&mut file))));
-            },
-            _ => return Err(PackageLoadError::UnknownPackage)
-        }
+		match try!(detect_package_format(&mut file)) {
+			PackageFormat::IWad | PackageFormat::PWad => {
+				self.add_package(&try!(wrap_wad_error(WadPackage::new(&mut file))));
+			},
+			_ => return Err(PackageLoadError::UnknownPackage)
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn clear_resources(&mut self) {
-        self.maps.clear();
-    }
+	pub fn clear_resources(&mut self) {
+		self.maps.clear();
+	}
 
-    fn add_package(&mut self, package : &WadPackage) {
-        self.maps.extend_from_slice(package.get_maps());
-    }
+	fn add_package(&mut self, package : &WadPackage) {
+		self.maps.extend_from_slice(package.get_maps());
+	}
 }
 
 fn detect_package_format(file : &mut File) -> PackageLoadResult<PackageFormat> {
-    match file.seek(SeekFrom::Start(0)) {
-        Err(error) => return Err(PackageLoadError::IoFailure(error)),
-        _ => ()
-    }
+	match file.seek(SeekFrom::Start(0)) {
+		Err(error) => return Err(PackageLoadError::IoFailure(error)),
+		_ => ()
+	}
 
-    let mut reader = BufReader::new(file);
+	let mut reader = BufReader::new(file);
 
-    // Check for an IWAD or PWAD signature
-    let mut signature = [0u8; 4];
-    if let Err(error) = reader.read_exact(&mut signature[..]) {
-        return Err(PackageLoadError::IoFailure(error));
-    }
+	// Check for an IWAD or PWAD signature
+	let mut signature = [0u8; 4];
+	if let Err(error) = reader.read_exact(&mut signature[..]) {
+		return Err(PackageLoadError::IoFailure(error));
+	}
 
-    if signature[1] == 'W' as u8 && signature[2] == 'A' as u8 && signature[3] == 'D' as u8 {
-        if signature[0] == 'I' as u8 {
-            return Ok(PackageFormat::IWad);
-        }
-        else if signature[0] == 'P' as u8 {
-            return Ok(PackageFormat::PWad);
-        }
-    }
+	if signature[1] == 'W' as u8 && signature[2] == 'A' as u8 && signature[3] == 'D' as u8 {
+		if signature[0] == 'I' as u8 {
+			return Ok(PackageFormat::IWad);
+		}
+		else if signature[0] == 'P' as u8 {
+			return Ok(PackageFormat::PWad);
+		}
+	}
 
-    Ok(PackageFormat::Unknown)
+	Ok(PackageFormat::Unknown)
 }
 
 fn wrap_wad_error<T>(error : WadResult<T>) -> PackageLoadResult<T> {
-    match error {
-        Ok(value) => Ok(value),
-        Err(WadError::IoFailure(error)) => Err(PackageLoadError::IoFailure(error)),
-        Err(error) => Err(PackageLoadError::WadError(error))
-    }
+	match error {
+		Ok(value) => Ok(value),
+		Err(WadError::IoFailure(error)) => Err(PackageLoadError::IoFailure(error)),
+		Err(error) => Err(PackageLoadError::WadError(error))
+	}
 }
